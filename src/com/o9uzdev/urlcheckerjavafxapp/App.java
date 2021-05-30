9177df3f37414793dc5b795760b3bc226349928b
o9uzdev <o9uzdev@gmail.com>
2021-05-30 15:46:39 +0300

    added project skeleton and module structure

---
+++ src/com/o9uzdev/urlcheckerjavafxapp/App.java
@@ -0,0 +1,17 @@
+package com.o9uzdev.urlcheckerjavafxapp;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        // TODO:
+        // Add form
+        // add label
+        // add textfield
+        // add button
+        // add EventHandler for button
+        // add alert dialog
+    }
+}
---
+++ src/module-info.java
@@ -0,0 +1,6 @@
+module URLCheckerJavaFx {
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.graphics;
+    opens com.o9uzdev.urlcheckerjavafxapp;
+}
