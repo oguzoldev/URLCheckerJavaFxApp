113a40a383bf1ea36ac8fa1b402a8d6e732c8100
o9uzdev <o9uzdev@gmail.com>
2021-05-31 15:44:52 +0300

    Added nodes

--- src/com/o9uzdev/urlcheckerjavafxapp/App.java
+++ src/com/o9uzdev/urlcheckerjavafxapp/App.java
@@ -1,6 +1,9 @@
 package com.o9uzdev.urlcheckerjavafxapp;
 
 import javafx.application.Application;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class App extends Application {
@@ -13,5 +16,10 @@ public class App extends Application {
         // add button
         // add EventHandler for button
         // add alert dialog
+
+        Label label = new Label("URL");
+        TextField textField = new TextField();
+        Button button = new Button("Check");
+
     }
 }
