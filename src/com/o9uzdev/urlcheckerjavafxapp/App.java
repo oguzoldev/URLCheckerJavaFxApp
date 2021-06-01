9ff7de224a694d13a06b848fd5df5a051eee7e6b
o9uzdev <o9uzdev@gmail.com>
2021-06-01 20:59:46 +0300

    Removed folder from repository

--- src/com/o9uzdev/urlcheckerjavafxapp/App.java
+++ src/com/o9uzdev/urlcheckerjavafxapp/App.java
@@ -1,9 +1,13 @@
 package com.o9uzdev.urlcheckerjavafxapp;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class App extends Application {
@@ -19,7 +23,19 @@ public class App extends Application {
 
         Label label = new Label("URL");
         TextField textField = new TextField();
+        textField.setPrefWidth(400);
         Button button = new Button("Check");
+
+        HBox hRoot = new HBox();
+        hRoot.setPadding(new Insets(10, 10, 10, 10));
+        hRoot.setSpacing(10);
+        hRoot.setAlignment(Pos.CENTER_LEFT);
+
+        hRoot.getChildren().addAll(label, textField, button);
+
+        stage.setTitle("URL Checker JavaFX App");
+        stage.setScene(new Scene(hRoot, 520, 70));
+        stage.show();
 
     }
 }
--- src/module-info.java
+++ src/module-info.java
@@ -2,5 +2,6 @@ module URLCheckerJavaFx {
     requires javafx.base;
     requires javafx.controls;
     requires javafx.graphics;
+
     opens com.o9uzdev.urlcheckerjavafxapp;
 }
