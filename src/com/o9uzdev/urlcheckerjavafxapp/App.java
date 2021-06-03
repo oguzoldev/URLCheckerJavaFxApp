255988d348fe1c1cbcea2d23350137dbaff9a4f7
o9uzdev <o9uzdev@gmail.com>
2021-06-03 19:46:29 +0300

    Added button EventHandler

--- src/com/o9uzdev/urlcheckerjavafxapp/App.java
+++ src/com/o9uzdev/urlcheckerjavafxapp/App.java
@@ -4,22 +4,22 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class App extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        // TODO:
-        // Add form
-        // add label
-        // add textfield
-        // add button
-        // add EventHandler for button
-        // add alert dialog
+
+        String regex = "^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
+        Alert alert = new Alert(Alert.AlertType.NONE);
 
         Label label = new Label("URL");
         TextField textField = new TextField();
@@ -28,9 +28,11 @@ public class App extends Application {
 
         // button EventHandler
         button.setOnAction(event -> {
-            // URL verification
-            String regex = "^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
-            System.out.println(IsMatch(textField.getText(), regex));
+            alert.setAlertType(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Alert");
+            alert.setHeaderText("URL Checker JavaFX App");
+            alert.setContentText("URL (true/false) :" + IsMatch(textField.getText(), regex));
+            alert.showAndWait();
         });
 
         HBox hRoot = new HBox();
@@ -43,6 +45,17 @@ public class App extends Application {
         stage.setTitle("URL Checker JavaFX App");
         stage.setScene(new Scene(hRoot, 520, 70));
         stage.show();
+
+    }
+
+    private static boolean IsMatch(String s, String pattern) {
+        try {
+            Pattern pattern2 = Pattern.compile(pattern);
+            Matcher matcher = pattern2.matcher(s);
+            return matcher.matches();
+        } catch (RuntimeException e) {
+            return false;
+        }
 
     }
 }
